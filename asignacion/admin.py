44975a1d81f26b3c30dfabc89b9b1a0a8066d431
unknown <evallardy@gmail.com>
2024-04-13 15:09:13 -0600

    version 1.001.012

--- asignacion/admin.py
+++ asignacion/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from armamento.models import *
+from asignacion.models import *
+from cliente.models import *
+# from contabilidad.models import *
+# from core.models import *
+from rh.models import *
+from usuario.models import *
+
+
+admin.site.register(Armamento)
+admin.site.register(Asignacion)
+admin.site.register(Cliente)
+admin.site.register(Empleado)
+admin.site.register(Generales)
+admin.site.register(Usuario)
+admin.site.register(Vehiculo)
